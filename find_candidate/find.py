a5ad637c7b886157c997f70273f66f7e546d1361
laochanlam <laochanlam@gmail.com>
2017-08-29 16:31:58 +0800

    increase to 10000 verison & basic completed

--- find_candidate/find.py
+++ find_candidate/find.py
@@ -30,10 +30,13 @@ def main():
     alt_allele = sys.argv[3]
     call_pos = int(sys.argv[2])
 
-    count_A = [0]*1000
-    count_G = [0]*1000
-    count_C = [0]*1000
-    count_T = [0]*1000
+    count_A = [0]*20000
+    count_G = [0]*20000
+    count_C = [0]*20000
+    count_T = [0]*20000
+    total_counts = [0]*20000
+    min_count = 2
+    candidate_count = 0
 
     for sam_line in sam:
         line = sam_line.split("\t")
@@ -48,7 +51,9 @@ def main():
         ref_pos = pos - call_pos + 200
         read_pos = 0
         timer = timer + 1
-        print "SAM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1"
+        # print "-----"
+        # print timer
+
         for cigar_index in range(len(cigar)):
             this_cigar = cigar[cigar_index]
             # print "INDEX NUMBER : %d" % cigar_index
@@ -56,6 +61,7 @@ def main():
             if this_cigar == 'M':
                 # print "COMPARING %s and %s" % (seq[read_pos], ref[ref_pos]) 
                 if seq[read_pos] == ref[ref_pos].upper():
+                    # print "right"
                     continue
                 else:
                     # print "GG"
@@ -85,26 +91,51 @@ def main():
             else:
                 continue
 
-        # if timer == 50:
+        # if timer == 1000:
+                # print i
+        # print "A"
+        # if count_A[i] != 0:
+        #     print "%d %d" % (i, count_A[i])
+        # print "G"
+        # if count_G[i] != 0:
+        #     print "%d %d" % (i, count_G[i])
+        # print "C"
+        # if count_C[i] != 0:
+        #     print "%d %d" % (i, count_C[i])
+        # print "T"
+        # if count_T[i] != 0:
+        #     print "%d %d" % (i, count_T[i])
+    for i in range(0,1000):
+        total_counts[i] = count_A[i] + count_C[i] + count_G[i] + count_T[i]
+        if total_counts != 0:
+            if count_A[i] >= min_count:
+                if count_A[i] / total_counts[i] >= 0.25:
+                    # print "A %d" % i
+                    candidate_count = candidate_count + 1
+            if count_G[i] >= min_count:
+                if count_G[i] / total_counts[i] >= 0.25:
+                    # print "G %d" % i
+                    candidate_count = candidate_count + 1
+            if count_C[i] >= min_count:
+                if count_C[i] / total_counts[i] >= 0.25:
+                    # print "C %d" % i
+                    candidate_count = candidate_count + 1
+            if count_T[i] >= min_count:
+                if count_T[i] / total_counts[i] >= 0.25:
+                    # print "T %d" % i
+                    candidate_count = candidate_count + 1
 
+    print candidate_count
+        
+                
+                
+                
+                
+                # print total_counts[i]
             # return 0
 
-    print "A"
-    for i in range(0,1000):
-        if count_A[i] != 0:
-            print i
-    print "G"
-    for i in range(0,1000):
-        if count_G[i] != 0:
-            print i
-    print "C"
-    for i in range(0,1000):
-        if count_C[i] != 0:
-            print i
-    print "T"
-    for i in range(0,1000):
-        if count_T[i] != 0:
-            print i
+
+        
                     
 
 if __name__ == "__main__":

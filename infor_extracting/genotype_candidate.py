3821c9543bad2be45b9311022883e99903c3defe
laochanlam <laochanlam@gmail.com>
2017-08-30 18:25:37 +0800

    change SNPs data structure into List

--- infor_extracting/genotype_candidate.py
+++ infor_extracting/genotype_candidate.py
@@ -50,15 +50,16 @@ def main():
 
     # Filter Version
     i = 0
-    SNPS = [0]* 824
+    SNPS = []
+
     for vcf_line in vcf:
         line_vcf = vcf_line.split("\t")
         if line_vcf[0][0] != '#':
             if (int(line_vcf[1]) <= 2000000 + 200) & (int(line_vcf[1]) >= 1000000 - 200):
                 if (len(line_vcf[3]) == 1) & (len(line_vcf[4]) == 1):
                     if int(line_vcf[0]) == 1:
-                        SNPS[i] = vcf_line
-                        i = i + 1
+                        SNPS.append(vcf_line)
+                        
 
     print "[Bounding Completed]"
 
@@ -68,47 +69,47 @@ def main():
         timer = 0
         for SNP_line in SNPS:
             timer = timer + 1
-            if SNP_line != "0":
-                line_SNP = SNP_line.split("\t")
+            print SNP_line
+            line_SNP = SNP_line.split("\t")
 
-                ref = line_SNP[3]
-                alt = line_SNP[4]
-                pos = line[1]
-                chrom = "chr" + line_SNP[0]
+            ref = line_SNP[3]
+            alt = line_SNP[4]
+            pos = line[1]
+            chrom = "chr" + line_SNP[0]
 
-                if (line_SNP[0][0] != '#') & (line_SNP[0] != "0"):
-                    # if chromosome == line_SNP[0]:
-                    # if (line[0] == line_SNP[4]):
-                    #     print "yo"
-                    if int(line_SNP[1]) < int(line[1]):
-                        continue
+            if (line_SNP[0][0] != '#') & (line_SNP[0] != "0"):
+                # if chromosome == line_SNP[0]:
+                # if (line[0] == line_SNP[4]):
+                #     print "yo"
+                if int(line_SNP[1]) < int(line[1]):
+                    continue
 
-                    # het or hom
-                    if (int(line[1]) == int(line_SNP[1])) & (line_SNP[4]  == line[0]):
-                        # print line[1]
-                        # print "vcf : %s and candidate : %s" % (line_SNP[4], line[0])
-                    
-                        if (line_SNP[9][0] == "1") & (line_SNP[9][2] == "1"):
-                            genotype = "hom-alt"
-                            print "hom-alt"
-                        else:
-                            genotype = "het"
-                            print "het"
-                        command = "../image_generation/gen_image.sh %s %s %s %s %s %s" % (chrom, pos, alt, bam, fa, genotype)
-                        subprocess.call(shlex.split(command))
+                # het or hom
+                if (int(line[1]) == int(line_SNP[1])) & (line_SNP[4]  == line[0]):
+                    # print line[1]
+                    # print "vcf : %s and candidate : %s" % (line_SNP[4], line[0])
+                
+                    if (line_SNP[9][0] == "1") & (line_SNP[9][2] == "1"):
+                        genotype = "hom-alt"
+                        # print "hom-alt"
+                    else:
+                        genotype = "het"
+                        # print "het"
+                    command = "../image_generation/gen_image.sh %s %s %s %s %s %s" % (chrom, pos, alt, bam, fa, genotype)
+                    subprocess.call(shlex.split(command))
 
-                        variant_count = variant_count + 1
-                        break
+                    variant_count = variant_count + 1
+                    break
 
 
-                    # Ref
-                    if int(line_SNP[1]) > int(line[1]):
-                        ref_count = ref_count + 1
-                        genotype = "ref"
-                        print "ref"
-                        command = "../image_generation/gen_image.sh %s %s %s %s %s %s" % (chrom, pos, alt, bam, fa, genotype)
-                        subprocess.call(shlex.split(command))
-                        break
+                # Ref
+                if int(line_SNP[1]) > int(line[1]):
+                    ref_count = ref_count + 1
+                    genotype = "ref"
+                    # print "ref"
+                    command = "../image_generation/gen_image.sh %s %s %s %s %s %s" % (chrom, pos, alt, bam, fa, genotype)
+                    subprocess.call(shlex.split(command))
+                    break
 
 
 
@@ -116,7 +117,7 @@ def main():
 
 
     # for SNPS_line in SNPS:
-    #     print SNPS_line
+        # print SNPS_line
 
     print variant_count
     print ref_count

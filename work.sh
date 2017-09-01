688399cca94dfc4df874c065f60acd9494db99a8
laochanlam <laochanlam@gmail.com>
2017-09-02 03:41:17 +0800

    path fixing

--- work.sh
+++ work.sh
@@ -5,9 +5,11 @@
 
 for (( i=1010000; i<=249000000; i=i+1000000 ))
 do
-    ../find_candidate/find_candidate.sh chr1 ${i} A ../data/elsa.bam ../data/ucsc.hg19.fasta >> ../find_candidate/candidate.txt
+    # Usage find_candidate <chr> <pos> A <bam> <fasta>
+    find_candidate/find_candidate.sh chr1 ${i} A data/elsa.bam data/ucsc.hg19.fasta > candidate.txt
     echo "$i find candidate completed!"
-    python genotype_candidate.py ../data/NA12878_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-Solid-10X_CHROM1-X_v3.3_highconf.vcf ../find_candidate/candidate.txt
+    # Usage label_classification.py <vcf> <candidate>
+    python label_classification/label_classification.py data/NA12878_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-Solid-10X_CHROM1-X_v3.3_highconf.vcf candidate.txt
     echo "$i generate img completed!"
 done
 echo "Completed"

605997541429e4de9912c69226aa344db3857ad8
laochanlam <laochanlam@gmail.com>
2017-09-02 03:43:39 +0800

    add some commit

--- find_candidate/find_candidate.sh
+++ find_candidate/find_candidate.sh
@@ -5,8 +5,7 @@ if [[ $# -lt 5 ]]; then
 fi
 name=`basename -s .bam $4`
 name=$name"_$1_$2"
-# echo $name
+# Generate range = 1000000 sam and fa, then delete it
 ~/samtools-1.5/samtools view $4 $1:$2-$(($2+1000000)) > $name.sam
 ~/samtools-1.5/samtools faidx $5 $1:$(($2-200))-$(($2+1000200)) | tail -n +2 > $name.fa
-#100dp?
 python ~/git/deepvariant/find_candidate/find.py $1 $name $2 $3

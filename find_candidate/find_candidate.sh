ebaff841fb9c7ff669416ce5840bffaf53c5f186
laochanlam <laochanlam@gmail.com>
2017-09-15 11:58:27 +0800

    relative path fixing

--- find_candidate/find_candidate.sh
+++ find_candidate/find_candidate.sh
@@ -7,8 +7,8 @@ name=`basename -s .bam $3`
 name=$name"_$1_$2"
 
 # Generate range = 1000000 sam and fa, then delete it
-tools/samtools-1.5/samtools view $3 $1:$2-$(($2+1000000)) > $name.sam
-tools/samtools-1.5/samtools faidx $4 $1:$(($2-200))-$(($2+1000200)) | tail -n +2 > $name.fa
-python ~/git/deepvariant/find_candidate/find.py $1 $name $2
+samtools view $3 $1:$2-$(($2+1000000)) > $name.sam
+samtools faidx $4 $1:$(($2-200))-$(($2+1000200)) | tail -n +2 > $name.fa
+python find_candidate/find.py $1 $name $2
 rm $name.sam
 rm $name.fa
--- image_generation/gen_image.sh
+++ image_generation/gen_image.sh
@@ -6,8 +6,8 @@ fi
 name=`basename -s .bam $4`
 name=$name"_$1_$2_$3_$6"
 echo "$name has been generated as a image [$6]" >> log_$1
-tools/samtools-1.5/samtools view $4 $1:$2-$2 > $name.sam
-tools/samtools-1.5/samtools faidx $5 $1:$(($2-110))-$(($2+110)) | tail -n +2 > $name.fa
+samtools view $4 $1:$2-$2 > $name.sam
+samtools faidx $5 $1:$(($2-110))-$(($2+110)) | tail -n +2 > $name.fa
 python image_generation/draw.py $name $2 $3 $6
 rm $name.sam
 rm $name.fa
